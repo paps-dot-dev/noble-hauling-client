7fe3ac44caec0b134a49c8d0c3f61483869859a1
Shawn <s.papineau92@gmail.com>
2023-09-13 14:24:19 -0400

    added google verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,10 @@ export default function RootLayout({
           property='og:image'
           content='https://us1-photo.nextdoor.com//business_logo/1c/18/1c18432c56afb75a12560b16a9d8df39.jpeg?request_version=v2&output_type=jpeg&sizing=linear&x_size=1&resize_type=resize&density=1'
         />
+        <meta
+          name='google-site-verification'
+          content='H9COaMH2cJpxDHUHIp44iNb-4iXeiQXrUdBJNf97afU'
+        />
       </head>
       <body className='from-gray-900 to-gray-600 bg-gradient-to-r'>
         <Header />
