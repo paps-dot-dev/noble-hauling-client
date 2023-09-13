94f55fc27eb3ceea39b653da4f1d7c08cb48d0c8
Shawn <s.papineau92@gmail.com>
2023-09-13 10:31:43 -0400

    added some meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
+      <head>
+        <meta
+          property='og:image'
+          content='https://us1-photo.nextdoor.com//business_logo/1c/18/1c18432c56afb75a12560b16a9d8df39.jpeg?request_version=v2&output_type=jpeg&sizing=linear&x_size=1&resize_type=resize&density=1'
+        />
+      </head>
       <body className='from-gray-900 to-gray-600 bg-gradient-to-r'>
         <Header />
         {children}
